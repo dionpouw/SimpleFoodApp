895696a823697414abfaa5fb4d9748704c727cdd
dionpouw <aldion.sumampouw@gmail.com>
2022-07-03 22:51:22 +0800

    Setup RecyclerView

--- app/src/main/java/com/jeflette/simplefoodapp/MainActivity.kt
+++ app/src/main/java/com/jeflette/simplefoodapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.jeflette.simplefoodapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeflette.simplefoodapp.adapter.FoodAdapter
+import com.jeflette.simplefoodapp.databinding.ActivityMainBinding
+import com.jeflette.simplefoodapp.util.FoodList
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Call the object class function to provide food list
+        val foodList = FoodList.provideFoodList()
+
+        // Set the recycler view with the food list & set the layout manager to linear layout
+        binding.rvFood.layoutManager = LinearLayoutManager(this)
+        binding.rvFood.adapter = FoodAdapter(foodList)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
